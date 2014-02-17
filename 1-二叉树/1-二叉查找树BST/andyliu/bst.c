11f8ab1c294d9685c454ff9f4a624ca3831c31d6
andyliu <hiandyliu@gmail.com>
2014-02-17 23:30:08 +0800

    add more

--- 1-二叉树/1-二叉查找树BST/andyliu/bst.c
+++ 1-二叉树/1-二叉查找树BST/andyliu/bst.c
@@ -1,4 +1,16 @@
+//andyliu create 2014-2-17
+//Binary search tree  in C lanuage
+
 #include <stdio.h>
+
+typedef int KEY_TYPE ;
+
+typedef struct Node{
+	KEY_TYPE key ;
+	struct Node* lchild ;
+	struct Node* rchild ;
+} ;
+
 
 int main(int argc, char *argv[]) {
 	printf("andy test\n");
