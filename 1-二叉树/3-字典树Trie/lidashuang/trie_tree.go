f52a3a374014d75fd4eb4a72b2810aa1a25880a2
lidashuang <ldshuang@gmail.com>
2014-03-08 21:30:42 +0800

    gofmt

--- 1-二叉树/3-字典树Trie/lidashuang/trie_tree.go
+++ 1-二叉树/3-字典树Trie/lidashuang/trie_tree.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-	"fmt"
+  "fmt"
 )
 
 type TrieTreeNode struct {
-	IsEnd bool
-	Count int
-	Key   rune
+  IsEnd bool
+  Count int
+  Key   rune
 
-	Next, Children *TrieTreeNode
+  Next, Children *TrieTreeNode
 }
 
 type TrieTree struct {
-	Root *TrieTreeNode
+  Root *TrieTreeNode
 }
 
 func NewTrieTreeNode(key rune) *TrieTreeNode {
-	node := &TrieTreeNode{
-		IsEnd:    false,
-		Count:    0,
-		Key:      key,
-		Next:     nil,
-		Children: nil,
-	}
-	return node
+  node := &TrieTreeNode{
+    IsEnd:    false,
+    Count:    0,
+    Key:      key,
+    Next:     nil,
+    Children: nil,
+  }
+  return node
 }
 
 func NewTrieTree() *TrieTree {
-	return &TrieTree{
-		Root: NewTrieTreeNode('z'),
-	}
+  return &TrieTree{
+    Root: NewTrieTreeNode('z'),
+  }
 }
 
 /*
@@ -64,56 +64,56 @@ func NewTrieTree() *TrieTree {
 */
 
 func (tree *TrieTree) Insert(str string) {
-	ptr := tree.Root
+  ptr := tree.Root
 
-	for _, ch := range str {
-		if ptr.Children == nil {
-			ptr.Children = NewTrieTreeNode(ch)
-			ptr = ptr.Children
-		} else {
-			if ptr.Children.Key != ch {
-				// 有兄弟结点并且相等
-				if ptr.Children.Next != nil && ptr.Children.Next.Key == ch {
-					ptr = ptr.Children.Next
-				} else {
-					// 创建兄弟节点
-					ptr.Children.Next = NewTrieTreeNode(ch)
-					ptr = ptr.Children.Next
-				}
-			} else {
-				// 相等
-				ptr = ptr.Children
-			}
-		}
-	}
+  for _, ch := range str {
+    if ptr.Children == nil {
+      ptr.Children = NewTrieTreeNode(ch)
+      ptr = ptr.Children
+    } else {
+      if ptr.Children.Key != ch {
+        // 有兄弟结点并且相等
+        if ptr.Children.Next != nil && ptr.Children.Next.Key == ch {
+          ptr = ptr.Children.Next
+        } else {
+          // 创建兄弟节点
+          ptr.Children.Next = NewTrieTreeNode(ch)
+          ptr = ptr.Children.Next
+        }
+      } else {
+        // 相等
+        ptr = ptr.Children
+      }
+    }
+  }
 
-	ptr.IsEnd = true
-	ptr.Count++
+  ptr.IsEnd = true
+  ptr.Count++
 }
 
 // 查询一个单词，返回单词个数
 func (tree *TrieTree) SearchWord(str string) int {
-	ptr := tree.Root
-	var noWord = 0
+  ptr := tree.Root
+  var noWord = 0
 
-	for _, ch := range str {
-		if ptr.Children == nil {
-			return noWord
-		}
+  for _, ch := range str {
+    if ptr.Children == nil {
+      return noWord
+    }
 
-		if ptr.Children.Key == ch {
-			ptr = ptr.Children
-			continue
-		}
+    if ptr.Children.Key == ch {
+      ptr = ptr.Children
+      continue
+    }
 
-		// 判断next
-		if ptr.Children.Next != nil && ptr.Children.Next.Key == ch {
-			ptr = ptr.Children.Next
-			continue
-		}
-		return noWord
-	}
-	return ptr.Count
+    // 判断next
+    if ptr.Children.Next != nil && ptr.Children.Next.Key == ch {
+      ptr = ptr.Children.Next
+      continue
+    }
+    return noWord
+  }
+  return ptr.Count
 }
 
 // todo
@@ -121,38 +121,38 @@ func (tree *TrieTree) RemoveWord(str string) {
 }
 
 func main() {
-	tree := NewTrieTree()
+  tree := NewTrieTree()
 
-	var strs = []string{"amy", "ann", "ann", "baa", "baa"}
-	for _, str := range strs {
-		tree.Insert(str)
-	}
+  var strs = []string{"amy", "ann", "ann", "baa", "baa"}
+  for _, str := range strs {
+    tree.Insert(str)
+  }
 
-	ptr := tree.Root
+  ptr := tree.Root
 
-	fmt.Printf("%c\n", ptr.Children.Children.Next.Children.Key)   // should n
-	fmt.Printf("%c\n", ptr.Children.Next.Key)                     // should b
-	fmt.Printf("%c\n", ptr.Children.Next.Children.Key)            // should a
-	fmt.Printf("%c\n", ptr.Children.Next.Children.Children.Key)   // should a
-	fmt.Printf("%d\n", ptr.Children.Next.Children.Children.Count) // should 2
-	fmt.Printf("%d\n", ptr.Children.Children.Next.Children.Count) // should 2
+  fmt.Printf("%c\n", ptr.Children.Children.Next.Children.Key)   // should n
+  fmt.Printf("%c\n", ptr.Children.Next.Key)                     // should b
+  fmt.Printf("%c\n", ptr.Children.Next.Children.Key)            // should a
+  fmt.Printf("%c\n", ptr.Children.Next.Children.Children.Key)   // should a
+  fmt.Printf("%d\n", ptr.Children.Next.Children.Children.Count) // should 2
+  fmt.Printf("%d\n", ptr.Children.Children.Next.Children.Count) // should 2
 
-	tree.Insert("ann")
+  tree.Insert("ann")
 
-	// test search
-	fmt.Println("====test search=====")
-	fmt.Printf("%d\n", tree.SearchWord("ann")) // should 3
-	fmt.Printf("%d\n", tree.SearchWord("baa")) // should 2
-	fmt.Printf("%d\n", tree.SearchWord("amy")) // should 1
+  // test search
+  fmt.Println("====test search=====")
+  fmt.Printf("%d\n", tree.SearchWord("ann")) // should 3
+  fmt.Printf("%d\n", tree.SearchWord("baa")) // should 2
+  fmt.Printf("%d\n", tree.SearchWord("amy")) // should 1
 
-	// other test
-	/*
-		var strs = []string{"amy", "amy", "amy", "ama"}
-		for _, str := range strs {
-			tree.Insert(str)
-		}
-		ptr := tree.Root
-		fmt.Printf("%d\n", ptr.Children.Children.Children.Count)      // should 3
-		fmt.Printf("%d\n", ptr.Children.Children.Children.Next.Count) // should 3
-	*/
+  // other test
+  /*
+  	var strs = []string{"amy", "amy", "amy", "ama"}
+  	for _, str := range strs {
+  		tree.Insert(str)
+  	}
+  	ptr := tree.Root
+  	fmt.Printf("%d\n", ptr.Children.Children.Children.Count)      // should 3
+  	fmt.Printf("%d\n", ptr.Children.Children.Children.Next.Count) // should 3
+  */
 }
