2df8fdae75bb6943018a4e23f81231b19d15ca39
icylogic <withguohao@gmail.com>
2014-03-09 22:14:29 +0800

    icylogic/fixed count, add free

--- 1-二叉树/3-字典树Trie/icylogic/la-1-3.c
+++ 1-二叉树/3-字典树Trie/icylogic/la-1-3.c
@@ -8,53 +8,58 @@ typedef struct Trie
   struct Trie** subtries;
 }Trie;
 
-Trie* Insert (Trie* root, char* word){
-	if (*word != NULL){
-		int i;
-		for (i = 0; *(root->subtries + i) != NULL; i++){
-			if ((*(root->subtries + i))->value == *word){
-				(*(root->subtries + i))->count++;
-				*(root->subtries + i) = Insert(*(root->subtries + i), ++word);
-				return root;
-			}
-		}
-
-		*(root->subtries + i + 1) = *(root->subtries + i);
-		Trie* subtrie = malloc(sizeof(Trie));
-		subtrie->value = *word;
-		subtrie->count = 1;
-		subtrie->subtries = (Trie**)malloc(sizeof(Trie*));
-		*(subtrie->subtries) = NULL;
-		subtrie = Insert(subtrie, ++word);
-		*(root->subtries + i) = subtrie;
+Trie* Create(char* word){
+    Trie* root = (Trie*)malloc(sizeof(Trie));
+    root->value = *word;
+    root->subtries = (Trie**)malloc(sizeof(Trie*));
+	if (*(word + 1) == NULL){
+		root->count = 1;
+        *(root->subtries) = NULL;
+	}
+	else {
+		root->count = 0;
+		*(root->subtries) = Create(++word);
+        *(root->subtries + 1) = NULL;
 	}
     return root;
 }
 
-int SubCountsSum (Trie* root){
-    int i, sum = 0;
-	for (i = 0; *(root->subtries + i) != NULL; i++){
-        sum += (*(root->subtries + i))->count;
+Trie* Insert (Trie* root, char* word){
+    int i;
+    for (i = 0; *(root->subtries + i) != NULL; i++){
+        if ((*(root->subtries + i))->value == *word){
+			if (*(word + 1) == NULL){
+				(*(root->subtries + i))->count++;
+			} else {
+                *(root->subtries + i) = Insert(*(root->subtries + i), ++word);
+			}
+            return root;
+        }
     }
-    return sum;
+
+    *(root->subtries + i + 1) = *(root->subtries + i);
+	*(root->subtries + i) = Create(word);
+    return root;
 }
 
 Trie* Remove (Trie* root, char* word){
     int i, oldCount;
 	for (i = 0; *(root->subtries + i) != NULL; i++){
 		if ((*(root->subtries + i))->value == *word){
-			if (*(++word) != NULL){
-                oldCount = (*(root->subtries + i))->count;
-                *(root->subtries + i) =  Remove(*(root->subtries + i), word);
-                if (oldCount > (*(root->subtries + i))->count){
-                    root->count--;
-                }
-            } else if ((*(root->subtries + i))->count > SubCountsSum(*(root->subtries + i))){		
-                (*(root->subtries + i))->count--;    //nothing happens when Remove "ab" in {"abnormal","abort"}
-				root->count--;
+			if (*(word + 1) == NULL){
+				if ((*(root->subtries + i))->count > 0){    //do nothing when removing "ab" from {"abort", "about"}
+					(*(root->subtries + i))->count--;
+				}
             }
-		}
-        return root;
+			else {
+				*(root->subtries + i) = Remove(*(root->subtries + i), ++word);
+			}
+            if ((*(root->subtries + i))->subtries == NULL){
+                    free(*(root->subtries + i));
+                    *(root->subtries + i) = NULL;
+            }
+            return root;
+        }
 	}
     return root;
 }
@@ -87,11 +92,12 @@ int main(){
 	root = Insert(root, "grab");
 	root = Insert(root, "adsence");
 
-	printf("%d", Query(root, "ab"));
-    root = Remove(root, "ab");
-	printf("%d", Query(root, "ab"));
-    root = Remove(root, "abnormal");
-	printf("%d", Query(root, "ab"));
+	printf("%d\n", Query(root, "ab"));
+	root = Remove(root, "ab");
+	printf("%d\n", Query(root, "ab"));
+	printf("%d\n", Query(root, "abnormal"));
+	root = Remove(root, "abnormal");
+	printf("%d\n", Query(root, "abnormal"));
 
     //Clear(root);
     root = NULL;
